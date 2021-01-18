7e8b3f9dda9e4d67e259d2ed1054ecb272c3e40f
Maria Tomeyan <mariatomeyan70@gmail.com>
2021-01-18 21:24:54 +0100

    - Modified users default table in Laravle - created addresses table to store addresses - created accounts table to account of the customer - created pivot table for user addresses

--- database/migrations/2021_01_18_201234_create_accounts_table.php
+++ database/migrations/2021_01_18_201234_create_accounts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAccountsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('accounts', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname');
-            $table->string('telephone');
+            $table->string('iban');
+            $table->string('owner');
+            $table->integer('customer_id');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('accounts');
     }
 }
